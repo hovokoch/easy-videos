6a504b9fcc628780a66674514974cbdaea9a9280
hovokoch <hovoqoch@gmail.com>
2022-01-19 16:40:45 +0400

    Custom post type for videos

--- index.php
+++ index.php
@@ -17,3 +17,12 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 define( 'EASY_VIDEOS_VERSION', '1.0.0' );
 define( 'EASY_VIDEOS_MAIN_FILE', __FILE__ );
 define( 'EASY_VIDEOS_URL', untrailingslashit( plugins_url( '', EASY_VIDEOS_MAIN_FILE ) ));
+
+/**
+ * Initialization
+ */
+function easy_videos_init()
+{
+    require_once 'easy_videos.php';
+}
+add_action('plugins_loaded', 'easy_videos_init');
