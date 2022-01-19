c9389b9e0a6e4f4cf28beb2fbb6edb3b65910993
hovokoch <hovoqoch@gmail.com>
2022-01-19 16:29:03 +0400

    Plugin index file with define variables

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Easy Videos
+ * Plugin URI: https://wordpress.org/plugins/easy-videos/
+ * Description: Easy Videos description
+ * Author: Easy Videos
+ * Author URI: https://easy-videos.com/
+ * Version: 1.0.0
+ * Text Domain: easy-videos
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Required constants
+ */
+define( 'EASY_VIDEOS_VERSION', '1.0.0' );
+define( 'EASY_VIDEOS_MAIN_FILE', __FILE__ );
+define( 'EASY_VIDEOS_URL', untrailingslashit( plugins_url( '', EASY_VIDEOS_MAIN_FILE ) ));
