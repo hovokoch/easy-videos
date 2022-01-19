ab21259f2903b630cea09bcaa71df872478bd81a
hovokoch <hovoqoch@gmail.com>
2022-01-19 16:50:12 +0400

    Custom taxonomy for videos

--- easy_videos.php
+++ easy_videos.php
@@ -15,6 +15,7 @@ class EasyVideos
         $this->api_key = get_option('easy_videos_api_key');
 
         add_action('init', [$this, 'easy_videos_post_type']);
+        add_action('init', [$this, 'easy_videos_taxonomies']);
     }
 
     /**
@@ -36,6 +37,43 @@ class EasyVideos
 
             )
         );
+    }
+
+    /**
+     * Register plugin taxonomies
+     */
+    function easy_videos_taxonomies()
+    {
+
+        $labels = array(
+            'name' => _x('Categories', 'taxonomy general name'),
+            'singular_name' => _x('Category', 'taxonomy singular name'),
+            'search_items' => __('Search Categories'),
+            'popular_items' => __('Popular Categories'),
+            'all_items' => __('All Categories'),
+            'parent_item' => null,
+            'parent_item_colon' => null,
+            'edit_item' => __('Edit Category'),
+            'update_item' => __('Update Category'),
+            'add_new_item' => __('Add New Category'),
+            'new_item_name' => __('New Category Name'),
+            'separate_items_with_commas' => __('Separate categories with commas'),
+            'add_or_remove_items' => __('Add or remove categories'),
+            'choose_from_most_used' => __('Choose from the most used categories'),
+            'menu_name' => __('Categories'),
+        );
+
+        // register the non-hierarchical taxonomy for videos
+        register_taxonomy('video_categories', 'video', array(
+            'hierarchical' => false,
+            'labels' => $labels,
+            'show_ui' => true,
+            'show_in_rest' => true,
+            'show_admin_column' => true,
+            'update_count_callback' => '_update_post_term_count',
+            'query_var' => true,
+            'rewrite' => array('slug' => 'category'),
+        ));
     }
 
 }
